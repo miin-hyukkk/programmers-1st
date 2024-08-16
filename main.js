5109f106bddfb12920475dc5ce117fb2fada3089
miin-hyukkk <ham9893@naver.com>
2024-08-16 18:00:53 +0900

    feat: 섹션 1 기능(베스트셀러 로드) 구현

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+import { loadBookList } from "./api/load";
+
 const searchMenu = document.querySelector(".search-menu");
 const searchOptions = document.getElementById("search-options");
 const searchToggle = document.getElementById("search-toggle");
 const toggleIcon = document.getElementById("toggle-icon");
+
 // 검색어 종류 설정
 searchMenu.addEventListener("click", function () {
   console.log(searchMenu);
@@ -38,4 +41,55 @@ document.addEventListener("click", function (event) {
     }
   }
 });
+
+// section1
+async function initializeSwiper() {
+  try {
+    const bestBooks = await loadBookList("Bestseller", 10, 1);
+
+    console.log("Bestseller", bestBooks);
+    const mainSwiperWrapper = document.querySelector(
+      ".swiper-wrapper.mainSlideWrapper"
+    );
+    const subSwiperWrapper = document.querySelector(
+      ".swiper-wrapper.subSlideWrapper"
+    );
+    const mainSlidesHtml = bestBooks.item
+      .map((book) => {
+        return `
+        <div class="swiper-slide" id="box">
+          <img id="best" src="${book.cover || "../img/exbook.png"}" alt="${
+          book.title
+        }" />
+          <div id="desc">
+            <h2>${book.title}</h2>
+            <p id="author">${book.author}</p>
+            <p id="price">${book.priceSales}</p>
+            <span>${book.description || "No description available"}</span>
+          </div>
+        </div>
+      `;
+      })
+      .join("");
+
+    const subSlidesHtml = bestBooks.item
+      .slice(1)
+      .map((book) => {
+        return `
+        <div class="swiper-slide">
+          <img id="sub" src="${book.cover || "../img/exbook.png"}" alt="${
+          book.title
+        }" />
+        </div>
+      `;
+      })
+      .join("");
+    mainSwiperWrapper.innerHTML = mainSlidesHtml;
+    subSwiperWrapper.innerHTML = subSlidesHtml;
+  } catch (error) {
+    console.error("Failed to load book list:", error);
+  }
+}
+window.onload = initializeSwiper;
+
 
