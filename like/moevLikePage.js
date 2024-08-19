c27dd1e60226b3a54eda9b7c9a96f7bc23e3f8eb
miin-hyukkk <ham9893@naver.com>
2024-08-19 12:49:33 +0900

    delete: 미사용js파일 삭제

--- like/moevLikePage.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".like-btn").addEventListener("click", function () {
-  console.log("hi");
-
-  window.location.href = "your-favorites-page.html"; // 여기에 좋아요 페이지의 URL을 입력하세요.
-});
